e60df87e4ce0da5875cea45a7152a58424a953ad
Henk van Oers <hvo.pm@xs4all.nl>
2008-07-03 20:49:34 +0000

    Import of HVOERS/Apache2-checkReferer-0.02 from CPAN.
    gitpan-cpan-distribution: Apache2-checkReferer
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         HVOERS/Apache2-checkReferer-0.02.tar.gz
    gitpan-cpan-author:       HVOERS
    gitpan-cpan-maturity:     released

--- lib/Apache2/checkReferer.pm
+++ lib/Apache2/checkReferer.pm
@@ -9,11 +9,11 @@ Apache2::checkReferer - Prevent most "deep linking"
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Apache2::RequestRec ();
 use Apache2::RequestUtil ();
